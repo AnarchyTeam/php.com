3e2ab3958138ad470c86146fd2447b9923860e2a
Luqman Sungkar <loekmansungkar@gmail.com>
2017-02-25 15:20:19 +0700

    setup app dan pdo

--- index.php
+++ index.php
@@ -6,4 +6,41 @@
  * Time: 1:33 PM
  */
 
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
 require 'vendor/autoload.php';
+
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$config['db'] = [
+    'host' => 'localhost',
+    'user' => 'root',
+    'password' => 'kodokbangkok',
+    'dbname' => 'flag_quiz'
+];
+
+$app = new Slim\App(['settings' => $config]);
+$container = $app->getContainer();
+
+$container['db'] = function ($c){
+    $db = $c['settings']['db'];
+    $pdo = new PDO("mysql:host=".$db['host'].";dbname=".$db['dbname'], $db['user'], $db['password']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
+$app->get('/', function (Request $request, Response $response){
+    $db = $this->db;
+    $statement = $db->query('select * from countries');
+
+    return print_r($statement->fetchAll(PDO::FETCH_ASSOC), 1);
+});
+
+$app->post('/', function (Request $request, Response $response){
+
+});
+
+$app->run();
