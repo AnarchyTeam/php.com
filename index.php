536b5df5e84f409df22c5be8d65ee4a63b4568fd
Luqman Sungkar <loekmansungkar@gmail.com>
2017-03-05 17:50:49 +0700

    tes

--- index.php
+++ index.php
@@ -124,8 +124,8 @@ $app->post('/', function (Request $request, Response $response){
             if(strtolower($text) == "mulai"){
                 $question = new Question($user);
 
-//                $bot->pushMessage($user_id, $question->generate());
-                $result = $bot->replyText($event['replyToken'], new TextMessageBuilder($question->generate(),1));
+                $result = $bot->pushMessage($user_id, $question->generate());
+//                $result = $bot->replyText($event['replyToken'], new TextMessageBuilder($question->generate(),1));
                 return $result->getHTTPStatus()." ".$result->getRawBody();
             }else{
                 $result = $bot->replyText($event['replyToken'], print_r($event, 1));
